7b3e77b94a17890049a6346a33c7c268fe87cf28
Jeff Ripke <jripke74@gmail.com>
2022-12-30 13:18:31 -0800

    add content

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const ExpenseItem = () => {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2022</div>
+      <div>
+        <h2>Car Inssurance</h2>
+        <div>$249.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
