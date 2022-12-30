6f69ef6fa075397220ac4054434ee337547e7cc1
Jeff Ripke <jripke74@gmail.com>
2022-12-30 14:07:58 -0800

    add data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 249.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Inssurance</h2>
-        <div className="expense-item__price">$249.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
